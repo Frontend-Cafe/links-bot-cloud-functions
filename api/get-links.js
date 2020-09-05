6c6100e24472d0a3f8453f5e5e5c9f38413f467a
Gian <giancarlobrusca@gmail.com>
2020-09-05 20:12:46 -0300

    bot-links-api 1

--- api/get-links.js
+++ api/get-links.js
@@ -1,9 +1,29 @@
-module.exports = (req, res) => {
-    const key = req.query.key;
-    const chat = key ? content[key] : "";
+const url = require("url");
+const MongoClient = require("mongodb").MongoClient;
 
-    res.json({
-        key: key,
-        body: chat,
-    });
+let cachedDb = null;
+
+async function connectToDatabase(uri) {
+  if (cachedDb) {
+    return cacheDb;
+  }
+
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+
+  const db = await client.db(url.parse(uri).pathname.substr(1));
+
+  cacheDb = db;
+  return db;
+}
+
+module.exports = async (req, res) => {
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+  const collection = await db.collection("links");
+  const links = await collection.find({}).toArray();
+
+  // const tags = req.query.tags
+
+  res.json({
+    body: links,
+  });
 };
